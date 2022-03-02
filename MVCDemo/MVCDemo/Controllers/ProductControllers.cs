a0450c979d8e7792219ac0e107414913d7fd639c
Bora TÜZÜN <boratzn94@gmail.com>
2022-03-02 23:15:38 +0300

    Add files via upload

---
+++ MVCDemo/MVCDemo/Controllers/ProductControllers.cs
@@ -0,0 +1,46 @@
+﻿using MVCDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDemo.Controllers
+{
+    internal class ProductControllers
+    {
+        SqlConnection _connection = new SqlConnection(@"Integrated Security = SSPI; Initial Catalog = Northwind; Data Source = BORA;");
+        public List<Product> GetAll(string categoryName)
+        {
+            List<Product> products = new List<Product>();
+            ConnectionControl();
+            SqlCommand command = new SqlCommand($"Select * from Northwind.dbo.Products p inner join Northwind.dbo.Categories c on p.CategoryId = c.CategoryId where c.CategoryName = '{categoryName}'", _connection);
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Product product = new Product
+                {
+                    ProductName = dataReader["ProductName"].ToString(),
+                    CategoryName = dataReader["CategoryName"].ToString()
+                };
+                products.Add(product);
+            }
+
+            dataReader.Close();
+            _connection.Close();
+
+            return products;
+        }
+
+        private void ConnectionControl()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+    }
+}
